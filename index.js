075f41f2fdf19814212c22dece17ac1985d7a3b4
federicoghedinastailamedia <federico.ghedina@stailamedia.com>
2016-11-17 23:07:57 +0100

    better error handling

--- index.js
+++ index.js
@@ -9,26 +9,20 @@ function malta_js_uglify(o, options) {
 	var self = this,
 		start = new Date(),
 		msg,
-		pluginName = path.basename(path.dirname(__filename)),
-		doErr = function (e) {
-			console.log(('[ERROR on ' + o.name + ' using ' + pluginName + '] :').red());
-			console.dir(e);
-			self.stop();
-		};
+		pluginName = path.basename(path.dirname(__filename));
 
 	options = options || {};
-	
 	options.fromString = true;
 
-	try{
+	try {
 		o.content = uglify_js.minify(o.content+"", options).code;
-	} catch(err) {
-		doErr(err);
+	} catch (err) {
+		self.doErr(err, o, pluginName);
 	}
 
 	return function (solve, reject){
 		fs.writeFile(o.name, o.content, function(err) {
-			err && doErr(err);
+			err && self.doErr(err, o, pluginName);
 			msg = 'plugin ' + pluginName.white() + ' wrote ' + o.name + ' (' + self.getSize(o.name) + ')';
 			solve(o);
 			self.notifyAndUnlock(start, msg);
