ad346138e1ca47439737fab00f4dff440a4a2d23
fedeghe <fedeghe@gmail.com>
2020-03-31 22:11:31 +0200

    rollback to old uglifyjs

--- index.js
+++ index.js
@@ -7,19 +7,18 @@ function malta_js_uglify(o, options) {
 	const self = this,
 		start = new Date(),
         pluginName = path.basename(path.dirname(__filename));
-        
+    
     let msg;
 
 	options = options || {};
 	options.fromString = true;
 	return (solve, reject) => {
         try {
-            const r = uglify_js.minify(o.content+"", options);
-                o.content = r.error ? o.content : r.code;
+            o.content = uglify_js.minify(o.content+"", options).code;
             fs.writeFile(o.name, o.content, err => {
                 err && self.doErr(err, o, pluginName);
-                msg = 'plugin ' + pluginName.white() + (r.error ? ' failed to write ' : ' wrote ') + o.name + ' (' + self.getSize(o.name) + ')';
-                
+                msg = 'plugin ' + pluginName.white() + (err ? ' failed to write ' : ' wrote ') + o.name + ' (' + self.getSize(o.name) + ')';
+
                 err
                     ? reject(`Plugin ${pluginName} write error:\n${err}`)
                     : solve(o);
@@ -30,6 +29,7 @@ function malta_js_uglify(o, options) {
             reject(`Plugin ${pluginName} uglifycation error:\n${err}`)
             self.notifyAndUnlock(start, msg);
         }
+		
 	};
 }
 malta_js_uglify.ext = ['js', 'coffee', 'ts'];
